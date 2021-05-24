a02f600c2a80a874287a7856ed17dafe642d0ae6
rawanvib <43780243+rawanvib@users.noreply.github.com>
2021-05-24 17:23:52 +0530

    changed the function which separates text

--- recipe dataset/pdf_text.py
+++ recipe dataset/pdf_text.py
@@ -5,11 +5,11 @@ import pymongo
 import pytesseract
 from pdf2image import convert_from_path
 import pdf_scorer
-from pdf2image.exceptions import (
-    PDFInfoNotInstalledError,
-    PDFPageCountError,
-    PDFSyntaxError
-)
+# from pdf2image.exceptions import (
+#     PDFInfoNotInstalledError,
+#     PDFPageCountError,
+#     PDFSyntaxError
+# )
 
 
 def pdf_to_image(directory_path, list_pdfs):
@@ -17,28 +17,26 @@ def pdf_to_image(directory_path, list_pdfs):
         os.makedirs(directory_path)
 
     for pdf in list_pdfs:
-        pages = convert_from_path(pdf, 300, first_page=7, fmt='jpg')
-        counter = 1
-        pdf_file_name = os.path.splitext(pdf)[0]
-
-        pdf_file_name = re.sub('/', '_', pdf_file_name)
-
-        for page in pages:
-            image_file_name = directory_path + pdf_file_name + '_' + str(counter) + '.jpg'
-            counter = counter + 1
-            page.save(image_file_name, 'JPEG')
-
+        pages = convert_from_path(pdf, 300, first_page=7, fmt='jpg', output_folder=directory_path)
+        # counter = 1
+        # pdf_file_name = os.path.splitext(pdf)[0]
+        #
+        # pdf_file_name = re.sub('/', '_', pdf_file_name)
+        #
+        # for page in pages:
+        #     image_file_name = directory_path + pdf_file_name + '_' + str(counter) + '.jpg'
+        #     counter = counter + 1
+        #     page.save(image_file_name, 'JPEG')
 
 
 def image_to_data(dir_name, col):
-
-
     for image_path in os.listdir(dir_name):
         image = cv2.imread(os.path.join(dir_name, image_path))
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 5)
 
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 25))
+
         dilation = cv2.dilate(thresh, rect_kernel, iterations=2)
 
         contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,6 +54,7 @@ def image_to_data(dir_name, col):
         text = pytesseract.image_to_string(cropped)
 
         single_document = {}
+
         single_document['title'] = text
 
         box = box[:-1]
@@ -88,39 +87,61 @@ def image_to_data(dir_name, col):
                 if sentence[0].isnumeric():
                     counter = counter + 1
 
-            if len_sentence == counter or len_sentence - 1 == counter or len_sentence - 2 == counter or len_sentence - 3 == counter:
-                ingredients.append(new_list)
+            # if len_sentence == counter or len_sentence -1 == counter or len_sentence - 2 == counter or len_sentence - 3 == counter:
+
+
+            if sentence[0].isupper():
+                count_full_stop_method = 0
+                for i in new_list:
+                    search_return = re.findall(r"\.", i)
+                    count_full_stop_method = count_full_stop_method + len(search_return)
+
+                if count_full_stop_method >= len_sentence//2 or count_full_stop_method==len_sentence:
+                    method.append(new_list)
+                else:
+                    if len_sentence == counter or len_sentence // 2 < counter:
+                        ingredients.append(new_list)
+                    else:
+                        method.append(new_list)
+
             else:
-                method.append(new_list)
+                if len_sentence == counter or len_sentence // 2 < counter:
+                    ingredients.append(new_list)
+                else:
+                    method.append(new_list)
 
-        single_document['ingredients']=ingredients
-        single_document['method']=method
 
-        #print(single_document)
+
+        single_document['ingredients'] = ingredients
+        single_document['method'] = method
+
+        # print(single_document)
         document_to_db(single_document, col)
-
+        single_document.clear()
 
 def document_to_db(data, col):
     col.insert_one(data)
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     my_client = pymongo.MongoClient("mongodb://localhost:27017/")
-    my_db = my_client['recipe_demo_1']
+    my_db = my_client['recipe_demo_changed_final2']
     my_col = my_db['dataset']
 
-
-    image_directory = 'images_folder/'
+    image_directory = 'images_folder2/'
     directory_name = "/home/webwerks/Desktop/ocr/pdfs"
 
     pdf_with_score = pdf_scorer.path_to_pdf_file(directory_name)
-
+    #print(pdf_with_score)
     list_pdf_with_good_score = []
     for pdf_file, score in pdf_with_score.items():
-        if score > 80:
+        if score > 80 :
             list_pdf_with_good_score.append(pdf_file)
-
 
     print(list_pdf_with_good_score)
     pdf_to_image(image_directory, list_pdf_with_good_score)
     image_to_data(image_directory, my_col)
+
+
+
+# image annotation
+#https://towardsdatascience.com/image-data-labelling-and-annotation-everything-you-need-to-know-86ede6c684b1
